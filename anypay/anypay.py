81c33816cd334516c6e381766748be934d5d3eb4
luminosoda <luminosoda@youngwaves.ru>
2020-08-19 02:48:46 +0500

    :sparkles: Base request method

--- anypay/anypay.py
+++ anypay/anypay.py
@@ -8,7 +8,10 @@ except ModuleNotFoundError:
     from json import loads
 
 # Typing
-from typing import Optional
+from typing import Mapping, Optional
+
+# Constants
+from .const import *
 
 
 __all__ = ("AnyPayAPI",)
@@ -28,3 +31,19 @@ class AnyPayAPI:
         self.secret = secret
 
         self._session = ClientSession(headers={"Accept": "application/json"})
+
+    async def _request(self, section: str, params: Mapping[str, str]) -> dict:
+        params_clear = dict()
+        for k, v in params.items():
+            if v is not None:
+                if v is float:
+                    v = str(v)
+
+                params_clear[k] = v
+
+        async with await self._session.get(
+            f"{ANYPAY_API_URL}/{section}/{self.id}", params=params
+        ) as response:
+            # The signature of the loads function from json doesn't match the signature from UJSON
+            # noinspection PyTypeChecker
+            return await response.json(loads=loads)
